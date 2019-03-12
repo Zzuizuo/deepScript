553568d66ae2ec7d02e123008e20067a6cfa5a43
Zzuizuo <Zzuizuo3094720053@163.com>
2019-03-12 13:53:15 +0800

    finish mvvm demo

--- mvvm.js
+++ mvvm.js
@@ -39,31 +39,128 @@ class Watcher { //观察者
         Dep.target = this
         let value = this.vm[this.exp]
         Dep.target = null
-        return
+        return value
     }
 }
 
+
 //compile
-class Mvvm {
-    constructor(data, el, exp) {
-        this.data = data
+class Compile {
+    constructor(el, vm) {
         this.el = el
-        this.exp = exp
+        this.vm = vm
+        this.app = document.querySelector(el)
+        this.frament = null
+    }
+    init() {
+        this.frament = this.nodeToFragment()
+        this.compileElement(this.frament)
+        this.app.appendChild(this.frament)
+    }
+    nodeToFragment() {
+        let frament = document.createDocumentFragment()
+        let child = this.app.firstChild
+        while (child) {
+            frament.appendChild(child)
+            child = this.app.firstChild
+        }
+        return frament
+    }
+    compileElement(el) {
+        let self = this
+        let childNodes = el.childNodes;
+        [...childNodes].forEach(node => {
+            let reg = /\{\{(.*)\}\}/
+            let text = node.textContent
+            console.log(node);
+
+            if (self.isElementNode(node)) {
+                self.compileAttr(node)
+            }
+
+            if (self.isTextNode(node) && reg.test(text)) { //判断指令
+                self.compileText(node, reg.exec(text)[1])
+            }
+            if (node.childNodes && node.childNodes.length) { //继续递归子节点
+                self.compileElement(node)
+            }
+        })
+    }
+    compileAttr(node) {
+        let self = this
+        console.log(node);
+
+        let attrs = node.attributes;
+        [...attrs].forEach(attr => {
+            let name = attr.name
+            if (self.isDirect(name)) {
+                let val = attr.value
+                if (name === 'v-model') {
+                    self.compileModel(node, val)
+                }
+            }
+        });
+    }
+    compileModel(node, val) {
+        let dataVal = this.vm[val]
+        this.updataModel(node, dataVal)
+
+        new Watcher(this.vm, val, (value) => { //添加到订阅者里面
+            this.updataModel(node, value);
+        });
+
+        node.addEventListener('input', (e) => {
+            let newVal = e.target.value
+            if (dataVal == newVal) {
+                return
+            }
+            this.vm[val] = newVal
+        })
+    }
+
+    compileText(node, exp) {
+        let self = this
+        let initText = this.vm[exp]
+        node.textContent = initText
+        this.updataText(node, initText) //初始化到视图
+        new Watcher(this.vm, exp, (val) => { //生成订阅器
+            self.updataText(node, val)
+        })
+    }
+    isDirect(name) {
+        return name.indexOf('v-') !== -1
+    }
+    updataModel(node, val) {
+        node.value = typeof val == 'undefined' ? '' : val
+    }
+    updataText(node, val) {
+        node.textContent = typeof val == 'undefined' ? '' : val
+    }
+    isElementNode(node) {
+        return node.nodeType === 1
+    }
+    isTextNode(node) {
+        return node.nodeType === 3
+    }
+}
+
+
+class Mvvm {
+    constructor(option) {
+        this.data = option.data
+        this.el = option.el
     }
     init() {
         let _self = this
         Object.keys(this.data).forEach(function(key) {
             _self.proxyKeys(key); // 绑定代理属性
         })
-        observe(_self.data)
-
-        _self.el.innerHTML = _self.data[_self.exp]
-        new Watcher(this.data, _self.exp, val => {
-            _self.el.innerHTML = val
-        })
-        return _self
+        observe(this.data)
+        let compile = new Compile(this.el, this.data)
+        compile.init()
+        return this
     }
-    proxyKeys(key) {
+    proxyKeys(key) { //代理
         var self = this;
         Object.defineProperty(this, key, {
             enumerable: false,
@@ -77,6 +174,7 @@ class Mvvm {
         });
     }
 }
+
 
 function defineReactive(obj, key, val) {
     observe(val) //递归对象
